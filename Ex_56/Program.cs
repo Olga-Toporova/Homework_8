a53fb03f1976454291b0b1e28d8775649ba4d44a
Olga Toporova <sova795@yandex.ru>
2022-09-01 13:52:30 +0500

    разобраны задачи 54,56,58

---
+++ Ex_56/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите количество строк m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество элементов массива в строке n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[m, n];
+        Random myRandom = new Random();
+        Console.WriteLine(" ");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = myRandom.Next(0, 10 + n);
+                Console.Write("{0}\t", array[i, j]);
+            }
+            Console.WriteLine(" ");
+        }
+        int result = 1;
+        int temp1 = 0, temp2 = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0)
+                {
+                    temp2 += array[i, j];
+                    temp1 = temp2;
+                }
+                else { temp2 += array[i, j]; }
+            }
+            if (temp1 > temp2)
+            {
+                result = i + 1;
+                temp1 = temp2;
+                temp2 = 0;
+            }
+            temp2 = 0;
+        }
+        Console.WriteLine(" ");
+        Console.Write("строка с наименьшей суммой элементов: {0}", result);
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+    }
+}
---
+++ Ex_58/Program.cs
@@ -0,0 +1,73 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+Console.WriteLine("Count rows first array");
+int rows=int.Parse(Console.ReadLine());
+Console.WriteLine("Count columns first array");
+int colimns=int.Parse(Console.ReadLine());
+
+int[,] matrix1=FillArray(rows,colimns);
+
+Console.WriteLine("Count rows second array");
+rows=int.Parse(Console.ReadLine());
+Console.WriteLine("Count columns second array");
+colimns=int.Parse(Console.ReadLine());
+
+int[,] matrix2=FillArray(rows,colimns);
+int[,] matrix3 = ProductMatrix(matrix1, matrix2);
+PrintArray(matrix1);
+Console.WriteLine();
+PrintArray(matrix2);
+Console.WriteLine();
+PrintArray(matrix3);
+
+
+int[,] FillArray(int n,int m)
+{
+    int[,] res=new int[n,m];
+    for(int i=0; i<n;i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            res[i,j]=new Random().Next(10);
+        }
+    
+    }
+    return res;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0 ; j < matrix.GetLength(1)  ; j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] ProductMatrix(int[,] matrix1, int[,] matrix2)
+{
+    if (matrix1.GetLength(1) != matrix2.GetLength(0))
+    {
+        System.Console.WriteLine("Матрицы невозможно перемножить в таком порядке!");
+        return matrix1;
+    }
+    int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                matrix3[i, j] = matrix3[i, j] + matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return matrix3;
+}
